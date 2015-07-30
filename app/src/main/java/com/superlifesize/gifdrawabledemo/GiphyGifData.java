236ffcc0dae28c1bcc9efbb1193a81e984c270c6
Nick Millward <nicholasmillward@gmail.com>
2015-07-30 14:04:06 -0400

    Initial commit.

---
+++ app/src/main/java/com/superlifesize/gifdrawabledemo/GiphyGifData.java
@@ -0,0 +1,18 @@
+package com.superlifesize.gifdrawabledemo;
+
+/**
+ * Created by nmilward on 3/26/15.
+ */
+public class GiphyGifData {
+
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
---
+++ app/src/main/java/com/superlifesize/gifdrawabledemo/MainActivity.java
@@ -0,0 +1,220 @@
+package com.superlifesize.gifdrawabledemo;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private GiphyGifData mGiphyGifData;
+    private ImageView imageView;
+    private pl.droidsonroids.gif.GifImageView mGifImageView;
+    private Button refresh_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.glide_gif);
+        mGifImageView = (pl.droidsonroids.gif.GifImageView) findViewById(R.id.gif_drawable);
+        refresh_button = (Button) findViewById(R.id.refresh_button);
+        refresh_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                refresh();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        refresh();
+    }
+
+    private void refresh() {
+        getGiphy();
+        loadRandomDrawable();
+    }
+
+    private void loadRandomDrawable() {
+        TypedArray images = getResources().obtainTypedArray(R.array.cat_images);
+        int randImage = (int) (Math.random() * images.length());
+        Log.i(TAG, "Random Image: " + randImage);
+
+        int image = images.getResourceId(randImage, R.drawable.cutecat001);
+        mGifImageView.setImageResource(image);
+        images.recycle();
+    }
+
+    private GiphyGifData getGif(String jsonData) throws JSONException {
+        JSONObject giphy = new JSONObject(jsonData);
+        JSONObject data = giphy.getJSONObject("data");
+
+        GiphyGifData gif = new GiphyGifData();
+        gif.setUrl(data.getString("image_url"));
+        Log.i(TAG, "Gif JSON Data - GIF URL: " + gif);
+
+        return gif;
+    }
+
+    private void updateDisplay() {
+        String gifUrl = mGiphyGifData.getUrl();
+        Log.i(TAG, "updateDisplay GIF URL: " + gifUrl);
+
+        Glide.with(MainActivity.this)
+                .load(gifUrl)
+                .thumbnail(0.1f)
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .centerCrop()
+                .into(new GlideDrawableImageViewTarget(imageView) {
+                    @Override
+                    public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> animation) {
+                        super.onResourceReady(resource, animation);
+                        //check isRefreshing
+                    }
+                });
+    }
+
+    private void getGiphy () {
+        //URL Format: http://api.giphy.com/v1/gifs/search?q=cute+cat&api_key=dc6zaTOxFJmzC&limit=1&offset=0
+        //Random Search URL: http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cute+funny+cat+kitten
+        String apiKey = "dc6zaTOxFJmzC"; //Giphy's Public API Key
+//        String limit = "50";
+//        String giphyUrl =
+//                "http://api.giphy.com/v1/gifs/search?q=" +
+//                "cute+funny+cat+kitten" +
+//                "&api_key=" +
+//                apiKey +
+//                "&limit=" +
+//                limit +
+//                "&offset=0"; //possibly add Search Term variable
+
+        String giphyUrl =
+                "http://api.giphy.com/v1/gifs/random" +
+                        "?api_key=" +
+                        apiKey +
+                        "&tag=" +
+                        "cat";
+
+
+        if (isNetworkAvailable()) {
+
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(giphyUrl)
+                    .build();
+
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+//                            toggleRefresh();
+                        }
+                    });
+                    Log.i(TAG, "Request Failure");
+                }
+
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                        }
+                    });
+                    try {
+                        String jsonData = response.body().string();
+                        if (response.isSuccessful()) {
+                            mGiphyGifData = getGif(jsonData);
+                            Log.v(TAG, "Giphy Gif Data from Response: " + mGiphyGifData);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    updateDisplay();
+                                }
+                            });
+                        } else {
+                            Log.i(TAG, "Response Unsuccessful");
+                        }
+                    }
+                    catch (IOException e) {
+                        Log.e(TAG, "Exception Caught: ", e);
+                    }
+                    catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(this, "Network is not available!", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
